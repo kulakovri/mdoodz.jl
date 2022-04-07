b44d58fc77f736b9d0767cd424c60b80bee686de
Roman Kulakov <roman.kulakov@ecapital.com>
2022-04-07 10:48:40 +0200

    add mdoodz.jl

--- src/generator.jl
+++ src/generator.jl
@@ -1,7 +1,7 @@
 using Clang.Generators
 using mdoodz_jll
 cd(@__DIR__)
-include_dir = mdoodz_jll.include_dir
+include_dir = normpath(mdoodz_jll.artifact_dir, "include")
 options = load_options(joinpath(@__DIR__, "generator.toml"))
 args = get_default_args()
 push!(args, "-I$include_dir")
