58c9908ea05a8ff01f95602f6ecb71b30f2e0df9
Roman Kulakov <roman.kulakov@ecapital.com>
2022-04-07 15:39:25 +0200

    fix comments

--- src/test.jl
+++ src/test.jl
@@ -2,21 +2,21 @@ include("mdoodz.jl")
 using Main.LibMdoodz
 
 function BuildInitialTopography(topo_chain::Ptr{markers}, model::params, scaling::scale)
-    # topograpy here
+    # topograpy routine
     return
 end
 
 const BuildInitialTopography_ptr = @cfunction(BuildInitialTopography, Cvoid, (Ptr{markers}, params, scale))
 
 function SetParticles(particles::Ptr{markers}, scaling::scale, model::params, materials::Ptr{mat_prop})
-    # topograpy here
+    # particles routine
     return
 end
 
 const SetParticles_ptr = @cfunction(SetParticles, Cvoid, (Ptr{markers}, scale, params, Ptr{mat_prop}))
 
 function SetBCs(mesh::Ptr{grid}, model::params, scaling::scale, particles::Ptr{markers}, materials::Ptr{mat_prop}, topo::Ptr{surface})
-    # set the BC
+    # Boundary conditions routine
     return
 end
 
